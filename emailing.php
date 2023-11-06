f4d6e5c1d972797e167768d1e997cb93cb0372d4
antoinel74 <antoine.lansman@gmail.com>
2023-11-06 14:48:35 +0100

    fix email source and subject

--- emailing.php
+++ emailing.php
@@ -11,19 +11,18 @@ $mail->Host = 'localhost';
 $mail->SMTPAuth = false;
 $mail->Port = 1025;
 
-$mail->setFrom('antoine.lansman@gmail.com', 'Admin');
-$mail->addAddress('antoine.lansman@gmail.com', 'User');
-$mail->Subject = 'Test Email';
+$mail->setFrom("$email", "Client");
+$mail->addAddress('support@example.com', 'Admin');
+$mail->Subject = "Support: $subject\n";
 
 $mail->Body = "First Name: $firstname\n";
 $mail->Body .= "Last Name: $name\n";
 $mail->Body .= "Country: $country\n";
 $mail->Body .= "Email: $email\n";
-$mail->Body .= "Subject: $subject\n";
 $mail->Body .= "Message:\n$message\n";
 
 if ($mail->send()) {
-    echo 'Email sent successfully';
+    echo 'Email sent successfully!';
 } else {
     echo 'Email could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
